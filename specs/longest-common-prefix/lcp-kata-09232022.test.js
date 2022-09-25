52c68a8a3cd95f1e5975a47e2c08f8d0d3f6b1d9
bklynate <nathanielcfa@gmail.com>
2022-09-24 21:34:32 -0400

    add new kata

---
+++ specs/longest-common-prefix/lcp-kata-09232022.test.js
@@ -0,0 +1,12 @@
+/*
+
+  Write a function to find the longest common prefix string amongst an array of strings.
+  If there is no common prefix, return an empty string "".
+
+*/
+
+function longestCommonPrefix(arr) {}
+
+test.skip('longestCommonPrefix', () => {
+  expect(longestCommonPrefix(['flower', 'flow', 'flight'])).toEqual('fl');
+});
